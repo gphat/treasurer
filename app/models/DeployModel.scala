1b7747fe33fba6a6e26bb4f61af3d09f35a07288
Cory G Watson <gphat@onemogin.com>
2014-04-02 14:50:36 -0700

    Add start of deploy stuff.

--- app/models/DeployModel.scala
+++ app/models/DeployModel.scala
@@ -1,6 +1,11 @@
 package models
 
+import anorm._
+import anorm.SqlParser._
 import org.joda.time.DateTime
+import play.api.db.DB
+import play.api.Play.current
+import util.AnormExtension._
 
 case class Deploy(
   id: Option[Long] = None,
@@ -12,4 +17,63 @@ case class Deploy(
 
 object DeployModel {
 
+  val getByIdQuery = SQL("SELECT * FROM deploys WHERE id={id} AND project_id={project_id}")
+  val deleteQuery = SQL("DELETE from deploys WHERE project_id={project_id} AND id={id}")
+  val insertQuery = SQL("INSERT INTO deploys (project_id, device, artifact_id, date_created, date_internal) VALUES ({project_id}, {device}, {artifact_id}, {date_created}, {date_internal})")
+
+  val deploy = {
+    get[Pk[Long]]("id") ~
+    get[String]("device") ~
+    get[String]("artifact_id") ~
+    get[DateTime]("date_created") ~
+    get[DateTime]("date_internal") map {
+      case id~device~artifactId~dateCreated~dateInternal => Deploy(
+        id = Some(id.get),
+        device = device,
+        artifactId = artifactId,
+        dateCreated = Some(dateCreated),
+        dateInternal = Some(dateInternal)
+      )
+    }
+  }
+
+  def create(projectId: Long, deploy: Deploy): Option[Deploy] = {
+
+    DB.withConnection { implicit conn =>
+      insertQuery.on(
+        'project_id -> projectId,
+        'device -> deploy.device,
+        'artifact_id -> deploy.artifactId,
+        'date_created -> deploy.dateCreated.getOrElse(new DateTime()),
+        'date_internal -> deploy.dateInternal.getOrElse(new DateTime())
+      ).executeInsert() map { id =>
+        getById(projectId, id)
+      } getOrElse { None }
+    }
+  }
+
+  /**
+   * Delete deploy.
+   */
+  def deleteById(projectId: Long, id: Long) {
+    DB.withConnection { implicit conn =>
+      deleteQuery.on(
+        'id -> id,
+        'project_id -> projectId
+      ).execute
+    }
+  }
+
+  /**
+   * Get a deploy by id.
+   */
+  def getById(projectId: Long, id: Long): Option[Deploy] = {
+
+    DB.withConnection { implicit conn =>
+      getByIdQuery.on(
+        'id -> id,
+        'project_id -> projectId
+      ).as(deploy.singleOpt)
+    }
+  }
 }
---
+++ test/DeployModelSpec.scala
@@ -0,0 +1,55 @@
+import models._
+
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+
+import play.api.libs.json.Json
+import play.api.test._
+import play.api.test.Helpers._
+
+@RunWith(classOf[JUnitRunner])
+class DeployModelSpec extends Specification {
+
+  "Deploy Model" should {
+
+    "create and delete deploys" in new WithApplication{
+      val proj = Project(
+        name = "Poop"
+      )
+
+      val project = ProjectModel.create(proj).get
+
+      val art = Artifact(
+        id = "abc123",
+        version = "1.2.3",
+        url = "http://www.example.com/poop.zip"
+      )
+
+      // Make artifact!
+      val artifactResult = ArtifactModel.create(project.id.get, art)
+      artifactResult must beSome
+      println(artifactResult.get.id)
+
+      val obj = Deploy(
+        device = "somehost",
+        artifactId = art.id
+      )
+
+      // Make deploy!
+      val createResult = DeployModel.create(project.id.get, obj)
+      createResult must beSome
+
+      // Clean up!
+      DeployModel.deleteById(project.id.get, createResult.get.id.get)
+
+      val oid = artifactResult.get.id
+      ArtifactModel.deleteById(project.id.get, oid)
+
+      // Make sure it's gone
+      ArtifactModel.getById(project.id.get, oid) must beNone
+
+      ProjectModel.deleteById(project.id.get)
+    }
+  }
+}
