55c2e24ce63cbd3fa4652f9d44cea0d8fbbe63c3
Cory G Watson <gphat@keen.io>
2014-07-14 14:41:33 -0700

    Compiles!

--- app/models/S3Model.scala
+++ app/models/S3Model.scala
@@ -3,12 +3,14 @@ package models
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3Client
+import play.api.Play
 import play.api.Play.current
 
 object S3 {
 
     val accessKeyId = Play.configuration.getString("treasurer.aws.access_key_id")
     val secretAccessKey = Play.configuration.getString("treasurer.aws.secret_access_key")
-    val awsCreds = new BasicAWSCredentials(accessKeyId, secretAccessKey)
+    // XXX Do something if missing!
+    val awsCreds = new BasicAWSCredentials(accessKeyId.get, secretAccessKey.get)
     lazy val s3Client = new AmazonS3Client(awsCreds);
 }
